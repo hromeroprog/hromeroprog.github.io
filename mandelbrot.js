3ff33d6a69f3e2e9908c17b7db52bb704dfcddbb
Hugo Romero <100386322@alumnos.uc3m.es>
2020-12-04 02:57:45 +0100

    Update mandelbrot.js

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     var canvas = $("canvas")[0];
     canvas.width = (window.innerWidth * 0.8);
+    canvas.height = (window.innerHeight * 0.5);
     alert(window.innerWidth);
     var c = canvas.getContext("2d");
 
 
-    //LINE
-    c.beginPath();
-    c.moveTo(40, 40);
-    c.lineTo(200, 100);
-    c.strokeStyle = "white";
-    c.stroke();
+    for (var col = 0; col < canvas.width; col++){
+        for(var row = 0; row < canvas.height; row++){
+            var greyscale = ((row+col)/(canvas.width+canvas.height))*255;
+            var color = "rgb(" + greyscale + "," + greyscale + "," + greyscale + ")";
+            c.fillStyle = color;
+            c.fillRect(col, row, 1, 1);
+        }
+    }
 });
 
