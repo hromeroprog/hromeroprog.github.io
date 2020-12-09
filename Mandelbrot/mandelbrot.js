74f11eeefa83a26e1621da22d8e898dc2ce29f76
Hugo Romero <100386322@alumnos.uc3m.es>
2020-12-09 15:59:10 +0100

    conseguido el efecto rectangulo en el canvas

--- Mandelbrot/mandelbrot.js
+++ Mandelbrot/mandelbrot.js
@@ -1,6 +1,7 @@
 var canvas = $("canvas")[0];
 var c = canvas.getContext("2d");
-var MAX_COMPUTE_PER_POINT = 20;
+var MAX_COMPUTE_PER_POINT_MAX_ITER = 100;
+var MAX_COMPUTE_PER_POINT;
 
 $(document).ready(async function(){
     var x_scaler = 0.6;
@@ -8,16 +9,65 @@ $(document).ready(async function(){
     var min_x = -2.2;
     var max_x = 1.6;
     var min_y = -(max_x-min_x)*(canvas.height/canvas.width)/2;
-
-    $('#colorpicker2').farbtastic({ callback: '#color2', width: 150 });
-    
-
-    for(var i = 0; i < 4; i++){
+    $(canvas).css("cursor", "wait");
+    for(var i = 1; i < 5; i++){
+        MAX_COMPUTE_PER_POINT = Math.floor(MAX_COMPUTE_PER_POINT_MAX_ITER*i/4);
         paintMandelbrot(min_x, max_x, min_y);
-        MAX_COMPUTE_PER_POINT+=20;
         await sleep(1);
     }
+    $(canvas).css("cursor", "pointer");
+    const cursor = $('.cursor');
+
+    var left_limit = $(canvas).offset().left + cursor.outerWidth()/2; 
+    var top_limit = $(canvas).offset().top + cursor.outerHeight()/2;
+    var right_limit = left_limit + $(canvas).outerWidth() - cursor.outerWidth();
+    var bottom_limit = top_limit + $(canvas).outerHeight() - cursor.outerHeight();
+    
+
+    $(function(){
+        $(window).resize(function(){
+            left_limit = $(canvas).offset().left + cursor.outerWidth()/2; 
+            top_limit = $(canvas).offset().top + cursor.outerHeight()/2;
+            right_limit = left_limit + $(canvas).outerWidth() - cursor.outerWidth();
+            bottom_limit = top_limit + $(canvas).outerHeight() - cursor.outerHeight();
+        });
+
+        $(canvas).mousemove(function(e){
+            cursor.show();
+            cursor.css("cursor", "none");
+            var x = e.pageX;
+            var y = e.pageY;
+
+            cursor.css({"left": x - cursor.outerWidth()/2, "top": y - cursor.outerHeight()/2});
+            if (x < left_limit) cursor.css("left", $(canvas).offset().left);
+            if (x > right_limit) cursor.css("left", $(canvas).offset().left + $(canvas).outerWidth() - cursor.outerWidth());
+            if (y < top_limit) cursor.css("top", $(canvas).offset().top);
+            if (y > bottom_limit) cursor.css("top", $(canvas).offset().top +$(canvas).outerHeight() - cursor.outerHeight());
+        });
+
+        $(cursor).mousemove(function(e){
+            var x = e.pageX;
+            var y = e.pageY;
+            
+            cursor.css({"left": x - cursor.outerWidth()/2, "top": y - cursor.outerHeight()/2});
+            if (x < left_limit) cursor.css("left", $(canvas).offset().left);
+            if (x > right_limit) cursor.css("left", $(canvas).offset().left + $(canvas).outerWidth() - cursor.outerWidth());
+            if (y < top_limit) cursor.css("top", $(canvas).offset().top);
+            if (y > bottom_limit) cursor.css("top", $(canvas).offset().top +$(canvas).outerHeight() - cursor.outerHeight());
+        });
+
+        $(cursor).mouseleave(function(e){
+            cursor.hide();
+        });
+
+        
+
+      
+    });
+
 });
+
+
 
 function setupCanvas(width_resize){
     canvas.width = (window.innerWidth * width_resize);
