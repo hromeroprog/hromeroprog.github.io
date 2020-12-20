e5640eed54a9e918ba1a18a02bfcb7576a776f84
Hugo Romero <100386322@alumnos.uc3m.es>
2020-12-21 00:52:32 +0100

    logo incorporado

--- Mandelbrot/prueba.js
+++ Mandelbrot/prueba.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+
+
 var canvas = $("canvas")[0];
 var c = canvas.getContext("2d");
 
@@ -6,3 +9,32 @@ canvas.height = $(".canvas_container").outerHeight();
 
 c.fillStyle = "red";
 c.fillRect(50,50, 50, 50);
+
+let x = 20;
+let y = 20;
+
+
+animate();
+
+function animate() {
+	requestAnimationFrame(animate);
+    clearCanvas();
+
+	c.beginPath();
+		c.arc(x, y, 20, 0, Math.PI * 2, false);
+		c.fillStyle = "blue";
+		c.fill();
+		c.stroke();
+    c.closePath();
+    x++;
+    y++;
+    y++;
+}
+
+function clearCanvas(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+
+});
