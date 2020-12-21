3446559b7835ee86908cb628b5cd35c8bcd71a8c
Hugo Romero <100386322@alumnos.uc3m.es>
2020-12-21 01:58:53 +0100

    pintando circulos animados

--- Mandelbrot/prueba.js
+++ Mandelbrot/prueba.js
@@ -1,40 +1,31 @@
 $(document).ready(function(){
-
-
 var canvas = $("canvas")[0];
 var c = canvas.getContext("2d");
 
 canvas.width = $(".canvas_container").outerWidth();
 canvas.height = $(".canvas_container").outerHeight();
 
-c.fillStyle = "red";
-c.fillRect(50,50, 50, 50);
-
-let x = 20;
-let y = 20;
-
+var c1 = new Circle(40,40,50,"red", 1, 2, c);
+var c2 = new Circle(150,200,50,"red", -1, 2, c);
+var circle_array = [c1, c2];
 
 animate();
 
 function animate() {
 	requestAnimationFrame(animate);
     clearCanvas();
+    
+    for(let circle of circle_array){
+        circle.draw();
+        circle.update();
+    }
+};
 
-	c.beginPath();
-		c.arc(x, y, 20, 0, Math.PI * 2, false);
-		c.fillStyle = "blue";
-		c.fill();
-		c.stroke();
-    c.closePath();
-    x++;
-    y++;
-    y++;
-}
+});
 
 function clearCanvas(){
+    var canvas = $("canvas")[0];
+    var c = canvas.getContext("2d");
     c.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
-
-});
